096a65bbbf31faea310186caaa60bca2c5ee6ec2
angelo-zamora <angelo.zamora@sanyutechnos-ph.com>
2025-01-23 10:58:52 +0800

    adjustment of update records and assigning schema on table

--- PG/PROCEDURES/順連携/proc_r4g_kojin_doitsunin_upd.sql
+++ PG/PROCEDURES/順連携/proc_r4g_kojin_doitsunin_upd.sql
@@ -2,7 +2,7 @@
 --  DDL for Procedure proc_r4g_kojin_doitsunin_upd
 --------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE proc_r4g_kojin_doitsunin_upd ( in_n_para12 IN numeric, io_c_err_code INOUT character varying, 
+CREATE OR REPLACE PROCEDURE dlgrenkei.proc_r4g_kojin_doitsunin_upd ( in_n_para12 IN numeric, io_c_err_code INOUT character varying, 
     io_c_err_text INOUT character varying )
 LANGUAGE plpgsql
 AS $$
@@ -11,7 +11,7 @@ AS $$
 /* 処理概要 : 個人宛名情報連携（同一人情報更新処理）                                                                      */
 /* 引数　　 : in_n_para12          … proc_kojin.パラメータ(12)                                                         */
 /*--------------------------------------------------------------------------------------------------------------------*/
-/* 履歴　　 : 新規作成                                                                                                  */
+/* 履歴     : 2025/01/23  CRESS-INFO.Angelo   001o006「住民情報（個人番号あり）」の取込を行う                           */
 /**********************************************************************************************************************/
 
 DECLARE
@@ -21,14 +21,14 @@ DECLARE
 
    cur_main CURSOR FOR
    SELECT LPAD( i_r4g_atena.atena_no::text, 15, '0' ) as atena_no
-   FROM i_r4g_atena
+   FROM dlgrenkei.i_r4g_atena
    WHERE result_cd = 2;
 
    cur_main_all CURSOR FOR
    SELECT kojin_no
    FROM f_kojin;
 	
-	rec_main                       i_r4g_atena%ROWTYPE;
+	rec_main                       dlgrenkei.i_r4g_atena%ROWTYPE;
 
 BEGIN
 
