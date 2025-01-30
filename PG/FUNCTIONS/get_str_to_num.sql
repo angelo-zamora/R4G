5fe13023073db5ea4c9357ea6d6da185c8523b21
john.mora <john.mora@sanyutechnos-ph.com>
2025-01-30 13:40:30 +0800

    code update

---
+++ PG/FUNCTIONS/get_str_to_num.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION get_str_to_num(in_str_num NUMERIC)
+RETURNS NUMERIC AS $$
+/**********************************************************************************************************************/
+/* 処理概要 : 文字列を数値型変換する                                                                                     */
+/* 引数     : in_str_num … 文字列                                                                                   　  */
+/* 戻り値　　: NUMERIC    … 変換後の数値                                                                                 */
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* 履歴     : 2025/01/30  CRESS-INFO.Drexler     新規作成     文字列から数値の変換を行う                                  */
+/**********************************************************************************************************************/
+
+BEGIN
+  　-- NULL、かつ空文字の場合は0を返す
+    CASE WHEN in_str_num IS NULL OR in_str_num = '' THEN
+        RETURN 0;
+    ELSE
+        RETURN in_str_num::numeric;
+    END CASE;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
