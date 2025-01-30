11282b56cf3abdac1dc6d8b1900c43525a16ea31
angelo-zamora <angelo.zamora@sanyutechnos-ph.com>
2025-01-30 09:21:44 +0800

    proc_tokusokuteishi_kibetsu implementation

--- PG/PROCEDURES/順連携/proc_r4g_tokusokuteishi_kibetsu.sql
+++ PG/PROCEDURES/順連携/proc_r4g_tokusokuteishi_kibetsu.sql
@@ -2,7 +2,7 @@
 --  DDL for Procedure proc_r4g_tokusokuteishi_kibetsu
 --------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE proc_r4g_tokusokuteishi_kibetsu (
+CREATE OR REPLACE PROCEDURE dlgrenkei.proc_r4g_tokusokuteishi_kibetsu (
    in_n_renkei_data_cd IN numeric, 
    in_n_renkei_seq IN numeric, 
    in_n_shori_ymd IN numeric, 
@@ -13,17 +13,18 @@ LANGUAGE plpgsql
 AS $$
 
 /**********************************************************************************************************************/
-/* 処理概要 : 督促停止期別情報（統合収滞納）                                                                               */
+/* 処理概要 : f_督促停止期別（f_tokusokuteishi_kibetsu）の追加／更新／削除を実施する                                  */
 /* 引数 IN  : in_n_renkei_data_cd … 連携データコード                                                                 */
 /*            in_n_renkei_seq     … 連携SEQ（処理単位で符番されるSEQ）                                               */
 /*            in_n_shori_ymd      … 処理日 （処理単位で設定される処理日）                                            */
-/*      OUT : io_c_err_code     … 例外エラー発生時のエラーコード                                                  */
-/*            io_c_err_text      … 例外エラー発生時のエラー内容                                                      */
+/*      OUT : io_c_err_code       … 例外エラー発生時のエラーコード                                                   */
+/*            io_c_err_text       … 例外エラー発生時のエラー内容                                                     */
 /*--------------------------------------------------------------------------------------------------------------------*/
-/* 履歴     :                                                                                                         */
+/* 履歴　　 :  CRESS-INFO.Angelo     新規作成     036o009「督促停止情報（統合収滞納）」の取込を行う                   */
 /**********************************************************************************************************************/
 
 DECLARE
+   rec_f_tokusokuteishi_kibetsu   f_tokusokuteishi_kibetsu%ROWTYPE;
    ln_shori_count                 numeric DEFAULT 0;
    ln_ins_count                   numeric DEFAULT 0;
    ln_upd_count                   numeric DEFAULT 0;
@@ -32,38 +33,43 @@ DECLARE
    lc_err_text                    character varying(100);
    ln_result_cd                   numeric DEFAULT 0;
    lc_err_cd                      character varying;
-   rec_log                        f_renkei_log%ROWTYPE;
+   rec_log                        dlgrenkei.f_renkei_log%ROWTYPE;
 
    ln_para01                      numeric DEFAULT 0;
-   lc_seq_no_tokusokuteishi       character varying;
    lc_kibetsu_key                 character varying;
    lc_sql                         character varying;
+
+   ln_result_cd_add               numeric DEFAULT 1; -- 追加
+   ln_result_cd_upd               numeric DEFAULT 2; -- 更新
+   ln_result_cd_err               numeric DEFAULT 9; -- エラー
+
+   lc_err_cd_normal               character varying = '0'; -- 通常
+   lc_err_cd_err                  character varying = '9'; -- エラー
     
    cur_parameter CURSOR FOR
    SELECT *
-   FROM f_renkei_parameter
+   FROM dlgrenkei.f_renkei_parameter
    WHERE renkei_data_cd = in_n_renkei_data_cd;
 
-   rec_parameter                  f_renkei_parameter%ROWTYPE;
+   rec_parameter                  dlgrenkei.f_renkei_parameter%ROWTYPE;
     
    cur_main CURSOR FOR
    SELECT *
-   FROM i_r4g_tokusoku_teishi_kibetsu
+   FROM dlgrenkei.i_r4g_tokusoku_teishi_kibetsu
    WHERE saishin_flg = '1'
       AND result_cd < 8;
 
-   rec_main                      i_r4g_tokusoku_teishi_kibetsu%ROWTYPE;
+   rec_main                      dlgrenkei.i_r4g_tokusoku_teishi_kibetsu%ROWTYPE;
     
    cur_lock CURSOR FOR
    SELECT * 
    FROM f_tokusokuteishi_kibetsu
-   WHERE seq_no_tokusokuteishi = lc_seq_no_tokusokuteishi
-      AND kibetsu_key = lc_kibetsu_key;
+   WHERE seq_no_tokusokuteishi = rec_main.tokusoku_teishi_kanri_no
+      AND kibetsu_key = rec_f_tokusokuteishi_kibetsu.kibetsu_key;
 
    rec_lock                      f_tokusokuteishi_kibetsu%ROWTYPE;
 
 BEGIN
-
    rec_log.proc_kaishi_datetime := CURRENT_TIMESTAMP;
    
    -- 1. パラメータ情報の取得
@@ -80,9 +86,9 @@ BEGIN
 
    -- 2. 連携先データの削除
    IF ln_para01 = 1 THEN
-   BEGIN
+      BEGIN
          SELECT COUNT(*) INTO ln_del_count FROM f_tokusokuteishi_kibetsu;
-         lc_sql := 'TRUNCATE TABLE dlgmain.f_tokusokuteishi_kibetsu;';
+         lc_sql := 'TRUNCATE TABLE dlgmain.f_tokusokuteishi_kibetsu';
          EXECUTE lc_sql;
       EXCEPTION
          WHEN OTHERS THEN
@@ -98,9 +104,6 @@ BEGIN
          RETURN;
       END IF;
 
-   -- 4. 桁数設定情報取得
-   -- r4gでは不要
-
    -- 5. 連携データの作成・更新
    ln_shori_count := 0;
    OPEN cur_main;
@@ -110,24 +113,43 @@ BEGIN
          EXIT WHEN NOT FOUND;
 
          ln_shori_count                 := ln_shori_count + 1;
-         lc_err_cd                      := '0';
+         lc_err_cd                      := lc_err_cd_normal;
          ln_result_cd                   := 0;
          lc_err_text                    := NULL;
          rec_lock                       := NULL;
          
-         lc_seq_no_tokusokuteishi := rec_main.tokusoku_teishi_kanri_no;
-         lc_kibetsu_key := get_kibetsu_key(
-                              rec_main.fuka_nendo::numeric
-                              , CASE WHEN rec_main.soto_nendo IS NOT NULL OR rec_main.soto_nendo <> '' THEN rec_main.soto_nendo::numeric ELSE 0 END
-                              , rec_main.zeimoku_cd --in_gyomu_shosai_cd
-                              --, get_r4g_code_conv(1, 3, null, rec_main.zeimoku_cd::character varying);
-                              , rec_main.kibetsu_cd
-                              , rec_main.tokucho_shitei_no
-                              , rec_main.jido_atena_no
-                              , rec_main.tsuchisho_no
-                              , CASE WHEN rec_main.jigyo_nendo_no IS NOT NULL OR rec_main.jigyo_nendo_no <> '' THEN rec_main.jigyo_nendo_no::numeric ELSE 0 END
-                              , CASE WHEN rec_main.shinkoku_rireki_no IS NOT NULL OR rec_main.shinkoku_rireki_no <> '' THEN rec_main.shinkoku_rireki_no::numeric ELSE 0 END
-                           );
+         -- 督促停止管理番号
+         rec_f_tokusokuteishi_kibetsu.seq_no_tokusokuteishi := rec_main.tokusoku_teishi_kanri_no;
+         -- 期別明細KEY
+         rec_f_tokusokuteishi_kibetsu.kibetsu_key := get_kibetsu_key(
+            rec_main.fuka_nendo,
+            rec_main.soto_nendo,
+            rec_main.zeimoku_cd,
+            rec_main.kibetsu_cd,
+            rec_main.tokucho_shitei_no,
+            rec_main.jido_atena_no,
+            rec_main.tsuchisho_no,
+            rec_main.jigyo_nendo_no,
+            rec_main.shinkoku_rireki_no
+         );
+         -- 督促停止年月日
+         rec_f_tokusokuteishi_kibetsu.tokusoku_teishi_ymd := get_date_num(to_date(rec_main.tokusoku_teishi_ymd, 'YYYY-MM-DD'));
+         -- 督促停止事由
+         rec_f_tokusokuteishi_kibetsu.tokusoku_kaijo_jiyu := rec_main.tokusoku_kaijo_jiyu::numeric;
+         -- 督促停止解除年月日
+         rec_f_tokusokuteishi_kibetsu.tokusoku_kaijo_ymd := get_date_num(to_date(rec_main.tokusoku_kaijo_ymd, 'YYYY-MM-DD'));
+         -- 督促停止解除事由
+         rec_f_tokusokuteishi_kibetsu.tokusoku_teishi_jiyu := rec_main.tokusoku_teishi_jiyu;
+         -- データ作成日時
+         rec_f_tokusokuteishi_kibetsu.ins_datetime := concat(rec_main.sosa_ymd, ' ', rec_main.sosa_time)::timestamp;
+         -- データ更新日時
+         rec_f_tokusokuteishi_kibetsu.upd_datetime := concat(rec_main.sosa_ymd, ' ', rec_main.sosa_time)::timestamp;
+         -- 更新担当者コード
+         rec_f_tokusokuteishi_kibetsu.upd_tantosha_cd := rec_main.sosasha_cd;
+         -- 更新端末名称
+         rec_f_tokusokuteishi_kibetsu.upd_tammatsu := 'SERVER';
+         -- 削除フラグ
+         rec_f_tokusokuteishi_kibetsu.del_flg := rec_main.del_flg::numeric;
          
          OPEN cur_lock;
             FETCH cur_lock INTO rec_lock;
@@ -148,64 +170,72 @@ BEGIN
                   , upd_tammatsu 
                   , del_flg
                ) VALUES (
-                  lc_seq_no_tokusokuteishi
-                  , lc_kibetsu_key
-                  , get_date_to_num(to_date(rec_main.tokusoku_teishi_ymd, 'yyyy-mm-dd'))
-                  , CASE WHEN rec_main.tokusoku_kaijo_jiyu IS NOT NULL OR rec_main.tokusoku_kaijo_jiyu <> '' THEN rec_main.tokusoku_kaijo_jiyu::numeric ELSE 0 END
-                  , get_date_to_num(to_date(rec_main.tokusoku_kaijo_ymd, 'yyyy-mm-dd'))
-                  , rec_main.tokusoku_teishi_jiyu
-                  , concat(rec_main.sosa_ymd, ' ', rec_main.sosa_time)::timestamp
-                  , concat(rec_main.sosa_ymd, ' ', rec_main.sosa_time)::timestamp
-                  , rec_main.sosasha_cd
-                  , 'SERVER'
-                  , CASE WHEN rec_main.del_flg IS NOT NULL OR rec_main.del_flg <> '' THEN rec_main.del_flg::numeric ELSE 0 END
+                  seq_no_tokusokuteishi
+                  , rec_f_tokusokuteishi_kibetsu.kibetsu_key
+                  , rec_f_tokusokuteishi_kibetsu.tokusoku_teishi_ymd
+                  , rec_f_tokusokuteishi_kibetsu.tokusoku_kaijo_jiyu
+                  , rec_f_tokusokuteishi_kibetsu.tokusoku_kaijo_ymd
+                  , rec_f_tokusokuteishi_kibetsu.tokusoku_teishi_jiyu
+                  , rec_f_tokusokuteishi_kibetsu.ins_datetime
+                  , rec_f_tokusokuteishi_kibetsu.upd_datetime
+                  , rec_f_tokusokuteishi_kibetsu.upd_tantosha_cd
+                  , rec_f_tokusokuteishi_kibetsu.upd_tammatsu 
+                  , rec_f_tokusokuteishi_kibetsu.del_flg
                );
 
                ln_ins_count := ln_ins_count + 1;
                lc_err_text := '';
-               lc_err_cd := '0';
-               ln_result_cd := 1;
-
-               EXCEPTION
-                  WHEN OTHERS THEN
-                     ln_err_count := ln_err_count + 1;
-                     lc_err_text := SUBSTRING( SQLERRM, 1, 100 );
-                     lc_err_cd := '9';
-                     ln_result_cd := 9;
-            END;
-         ELSE
-            BEGIN
-               UPDATE f_tokusokuteishi_kibetsu
-                  SET tokusoku_teishi_ymd = get_date_to_num(to_date(rec_main.tokusoku_teishi_ymd, 'yyyy-mm-dd'))
-                  , tokusoku_kaijo_jiyu = CASE WHEN rec_main.tokusoku_kaijo_jiyu IS NOT NULL OR rec_main.tokusoku_kaijo_jiyu <> '' THEN rec_main.tokusoku_kaijo_jiyu::numeric ELSE 0 END                  , tokusoku_kaijo_ymd = get_date_to_num(to_date(rec_main.tokusoku_kaijo_ymd, 'yyyy-mm-dd'))
-                  , tokusoku_teishi_jiyu = rec_main.tokusoku_teishi_jiyu
-                  , upd_datetime = CURRENT_TIMESTAMP
-                  , upd_tantosha_cd = rec_main.sosasha_cd
-                  , upd_tammatsu = 'SERVER'
-                  , del_flg = CASE WHEN rec_main.del_flg IS NOT NULL OR rec_main.del_flg <> '' THEN rec_main.del_flg::numeric ELSE 0 END
-				  WHERE seq_no_tokusokuteishi = lc_seq_no_tokusokuteishi
-                  AND kibetsu_key = lc_kibetsu_key;
-
-                  ln_upd_count := ln_upd_count + 1;
-                  lc_err_text := '';
-                  lc_err_cd := '0';
-                  ln_result_cd := 2;
+               lc_err_cd := lc_err_cd_normal;
+               ln_result_cd := ln_result_cd_add;
 
             EXCEPTION
                WHEN OTHERS THEN
                   ln_err_count := ln_err_count + 1;
                   lc_err_text := SUBSTRING( SQLERRM, 1, 100 );
-                  lc_err_cd := '9';
-                  ln_result_cd := 9;
+                  lc_err_cd := lc_err_cd_err;
+                  ln_result_cd := ln_result_cd_err;
             END;
+         ELSE
+            BEGIN
+               UPDATE f_tokusokuteishi_kibetsu
+                  SET tokusoku_teishi_ymd = rec_f_tokusokuteishi_kibetsu.tokusoku_teishi_ymd
+                  , tokusoku_kaijo_jiyu = rec_f_tokusokuteishi_kibetsu.tokusoku_kaijo_jiyu
+                  , tokusoku_kaijo_ymd = rec_f_tokusokuteishi_kibetsu.tokusoku_kaijo_ymd
+                  , tokusoku_teishi_jiyu = rec_f_tokusokuteishi_kibetsu.tokusoku_teishi_jiyu
+                  , upd_datetime = rec_f_tokusokuteishi_kibetsu.upd_datetime
+                  , upd_tantosha_cd = rec_f_tokusokuteishi_kibetsu.upd_tantosha_cd
+                  , upd_tammatsu = rec_f_tokusokuteishi_kibetsu.upd_tammatsu 
+                  , del_flg = rec_f_tokusokuteishi_kibetsu.del_flg
+               WHERE seq_no_tokusokuteishi = rec_f_tokusokuteishi_kibetsu.seq_no_tokusokuteishi
+                  AND kibetsu_key = rec_f_tokusokuteishi_kibetsu.kibetsu_key;
+
+                  ln_upd_count := ln_upd_count + 1;
+                  lc_err_text := '';
+                  lc_err_cd := lc_err_cd_normal;
+                  ln_result_cd := ln_result_cd_upd;
+
+            EXCEPTION
+               WHEN OTHERS THEN
+                  ln_err_count := ln_err_count + 1;
+                  lc_err_text := SUBSTRING( SQLERRM, 1, 100 );
+                  lc_err_cd := lc_err_cd_err;
+                  ln_result_cd := ln_result_cd_err;
+            END;
+         END IF;
+
+         -- 中間テーブルの「削除フラグ」が「1」のデータは「3：削除」を指定
+         IF rec_main.del_flg::numeric = 1 THEN
+            ln_result_cd := ln_result_cd_del;
          END IF;
 
          BEGIN
             -- 中間テーブル更新
-            UPDATE i_r4g_tokusoku_teishi_kibetsu 
-            SET result_cd = ln_result_cd
-               , error_cd = lc_err_cd
-               , error_text = lc_err_text
+            UPDATE dlgrenkei.i_r4g_tokusoku_teishi_kibetsu 
+            SET result_cd      = ln_result_cd
+               , error_cd      = lc_err_cd
+               , error_text    = lc_err_text
+               , seq_no_renkei = in_n_renkei_seq
+               , shori_ymd     = in_n_shori_ymd
             WHERE shikuchoson_cd = rec_main.shikuchoson_cd
                AND tokusoku_teishi_kanri_no = rec_main.tokusoku_teishi_kanri_no
                AND fuka_nendo = rec_main.fuka_nendo
@@ -219,11 +249,7 @@ BEGIN
                AND jido_atena_no = rec_main.jido_atena_no
                AND atena_no = rec_main.atena_no;
          EXCEPTION
-            WHEN OTHERS THEN
-               ln_err_count := ln_err_count + 1;
-               lc_err_text := SUBSTRING( SQLERRM, 1, 100 );
-               lc_err_cd := '9';
-               ln_result_cd := 9;
+            WHEN OTHERS THEN NULL;
          END;
 
       END LOOP;
@@ -238,7 +264,7 @@ BEGIN
    rec_log.proc_err_count := ln_err_count;
    
    -- データ連携ログ更新
-   CALL proc_upd_log(rec_log, io_c_err_code, io_c_err_text);
+   CALL dlgrenkei.proc_upd_log(rec_log, io_c_err_code, io_c_err_text);
 
    RAISE NOTICE 'レコード数: % | 登録数: % | 更新数: % | 削除数: % | エラー数: % ', ln_shori_count, ln_ins_count, ln_upd_count, ln_del_count, ln_err_count;
 
