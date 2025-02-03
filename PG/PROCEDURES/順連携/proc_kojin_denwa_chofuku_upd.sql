52e1e8e5581eb32672ae6b4fe2f8c825b4ec1f38
john.mora <john.mora@sanyutechnos-ph.com>
2025-02-03 13:15:43 +0800

    code update

--- PG/PROCEDURES/順連携/proc_kojin_denwa_chofuku_upd.sql
+++ PG/PROCEDURES/順連携/proc_kojin_denwa_chofuku_upd.sql
@@ -25,37 +25,32 @@ DECLARE
    lc_err_cd                           character varying;
    
 BEGIN
-    BEGIN
-        UPDATE f_denwa
-        SET (del_flg, yusen_flg) = (
+    UPDATE f_denwa
+    SET (del_flg, yusen_flg) = (
+        SELECT 
+            CASE  A.NUM  WHEN 1 THEN  0  ELSE 
+            CASE RENKEI_FLG  WHEN 0 THEN  0  ELSE 1  END  END
+            ,CASE  A.NUM  WHEN 1 THEN  A.MAX_YUSEN_FLG  ELSE 0  END
+        FROM (
             SELECT 
-                CASE  A.NUM  WHEN 1 THEN  0  ELSE 
-                CASE RENKEI_FLG  WHEN 0 THEN  0  ELSE 1  END  END
-                ,CASE  A.NUM  WHEN 1 THEN  A.MAX_YUSEN_FLG  ELSE 0  END
-            FROM (
-                SELECT 
-                    busho_cd,
-                    kojin_no,
-                    remban,
-                    renkei_flg,
-                    ROW_NUMBER() OVER(PARTITION BY busho_cd, kojin_no, denwa_no ORDER BY CASE WHEN renkei_flg = 0 THEN 0 ELSE 1 END, yusen_flg DESC, remban DESC) AS num,
-                    MAX(yusen_flg) OVER(PARTITION BY busho_cd, kojin_no, denwa_no) AS max_yusen_flg
-                FROM 
-                    f_denwa
-                WHERE 
-                    del_flg = 0
-            ) A
+                busho_cd,
+                kojin_no,
+                remban,
+                renkei_flg,
+                ROW_NUMBER() OVER(PARTITION BY busho_cd, kojin_no, denwa_no ORDER BY CASE WHEN renkei_flg = 0 THEN 0 ELSE 1 END, yusen_flg DESC, remban DESC) AS num,
+                MAX(yusen_flg) OVER(PARTITION BY busho_cd, kojin_no, denwa_no) AS max_yusen_flg
+            FROM 
+                f_denwa
             WHERE 
-                f_denwa.busho_cd = A.busho_cd
-                AND f_denwa.kojin_no = A.kojin_no
-                AND f_denwa.remban = A.remban
-        )
-        WHERE del_flg = 0;
-
-        EXCEPTION
-            WHEN OTHERS THEN NULL;
-        END;
-
+                del_flg = 0
+        ) A
+        WHERE 
+            f_denwa.busho_cd = A.busho_cd
+            AND f_denwa.kojin_no = A.kojin_no
+            AND f_denwa.remban = A.remban
+    )
+    WHERE del_flg = 0;
+        
 EXCEPTION
 WHEN OTHERS THEN
     io_c_err_code := SQLSTATE;
